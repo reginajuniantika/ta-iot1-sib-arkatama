0ae3e1433ede84fa76daf5ae2d15a4d60a3b79f7
“Regina” <reginajuniantika124@gmail.com>
2024-06-10 01:22:40 +0700

    new

--- routes/api.php
+++ routes/api.php
@@ -34,8 +34,8 @@ Route::group(['as' =>'api.'], function (){
 
 });
 
-Route::get('/data', [DataSensorController::class, 'index']);
-Route::post('/data', [DataSensorController::class, 'store']);
-Route::get('/data/{id}', [DataSensorController::class, 'show']);
+Route::get('/data', [DataSensorcontroller::class, 'index']);
+Route::post('/data', [DataSensorcontroller::class, 'store']);
+Route::get('/data/{id}', [DataSensorcontroller::class, 'show']);
 
 
